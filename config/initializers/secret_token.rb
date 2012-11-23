e35452a20e1d52bfc77ff251a9858a91ce3b9221
Simon George <simon@sfcgeorge.co.uk>
2012-11-23 21:51:09 +0000

    Set up Refinery then Forem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QLAQ::Application.config.secret_token = '0fff16b0bc98d9f1c845b04000b7b89505f4e3621204fc1d3a4bb0d103aa400534d1cc8e9a6751f309499ce56a4f1bd0015f569e9e76731bd9b4ac185b8b4f32'
