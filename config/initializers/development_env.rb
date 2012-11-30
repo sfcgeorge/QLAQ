512c01233a245468fecb5a3781fd5d93a419069c
Simon George <simon@sfcgeorge.co.uk>
2012-11-30 19:35:43 +0000

    Add privacy policy and cookie notice.

--- config/initializers/development_env.rb
+++ config/initializers/development_env.rb
@@ -1,3 +1,4 @@
 unless Rails.env.production?
   ENV['SECRET_CODE'] = "secret"
+  ENV['IUBUENDA_POLICY'] = 'http://www.iubenda.com/privacy-policy' #in production replace this URL with your privacy policy URL
 end
