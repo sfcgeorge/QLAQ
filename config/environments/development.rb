50b1d2ef90424bda794c5c182048aee347763ee5
Simon George <simon@sfcgeorge.co.uk>
2013-03-17 19:45:51 +0000

    Turn on email, add Mandrill, upgrade to Ruby 2.0.0.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,13 @@ QLAQ::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+
+  config.action_mailer.default_url_options = { :host => 'qlaq.dev' }
+
+  #config.action_mailer.perform_deliveries = false
+
+  config.action_mailer.delivery_method = :file
+
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ QLAQ::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'qlaq.co.uk' }
 
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = false
 
